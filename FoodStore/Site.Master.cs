79e87240ee3a6fde198dc66fc4743e2b44baa6ea
Shailendra2995 <skarki5277@conestogac.on.ca>
2024-08-03 13:02:56 -0400

    Corrected Everything

--- FoodStore/Site.Master.cs
+++ FoodStore/Site.Master.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.HtmlControls;
@@ -15,96 +17,21 @@ namespace FoodStore
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            // Check if the user is logged in by checking for the presence of the cookie
-            HttpCookie userCookie = Request.Cookies["UserInfo"];
-            bool isLoggedIn = userCookie != null;
-
-            // Clear existing navigation items
-            navList.Controls.Clear();
-
-            // Show only Logout button if the user is an admin
-            if (isLoggedIn)
-            {
-                if (userCookie["Type"] == "admin")
-                {
-                    AddLogoutItem(); // Adds the Logout link
-                }
-                else
-                {
-                    // Add standard navigation items for regular users
-                    AddNavItem("Foods.aspx", "Foods");
-                    AddNavItem("Cart.aspx", "Cart");
-                    AddLogoutItem(); // Add Logout link for non-admin users as well
-                }
-            }
-            else
-            {
-                // Add navigation items for non-logged-in users
-                AddNavItem("Home.aspx", "Home");
-                AddNavItem("Login.aspx", "Login");
-                AddNavItem("Register.aspx", "Register");
-            }
-
-            // Highlight the current page in the navigation
-            HighlightCurrentPage();
-        }
-
-        private void AddNavItem(string url, string text)
-        {
-            // Create a new list item (li)
-            var li = new HtmlGenericControl("li");
-            li.Attributes["class"] = "nav-item";
-
-            // Create a hyperlink (a) with the specified URL and text
-            var a = new HyperLink
-            {
-                NavigateUrl = url,
-                CssClass = "nav-link text",
-                Text = text
-            };
-
-            // Add the hyperlink to the list item
-            li.Controls.Add(a);
-
-            // Add the list item to the navigation list
-            navList.Controls.Add(li);
-        }
-
-        private void AddLogoutItem()
-        {
-            // Create a new list item (li) for logout
-            var liLogout = new HtmlGenericControl("li");
-            liLogout.Attributes["class"] = "nav-item";
-
-            // Create a hyperlink (a) for logout
-            var aLogout = new HyperLink
-            {
-                NavigateUrl = "Logout.aspx",
-                CssClass = "nav-link text",
-                Text = "Logout"
-            };
-
-            // Add the hyperlink to the list item
-            liLogout.Controls.Add(aLogout);
-
-            // Add the list item to the navigation list
-            navList.Controls.Add(liLogout);
-        }
-
-        private void HighlightCurrentPage()
-        {
-            // Highlight the current page in the navigation
             foreach (Control ctl in navList.Controls)
             {
-                if (ctl is HtmlGenericControl li && li.Controls[0] is HyperLink a)
+                if (!(ctl is HtmlGenericControl))
+                    continue;
+
+                var li = (HtmlGenericControl)ctl;
+                var a = (HyperLink)li.Controls[1];
+
+                if (Page.AppRelativeVirtualPath.Contains(a.NavigateUrl))
                 {
-                    if (Page.AppRelativeVirtualPath.IndexOf(a.NavigateUrl, StringComparison.OrdinalIgnoreCase) >= 0)
-                    {
-                        a.CssClass += " active";
-                        break;
-                    }
+                    a.CssClass += " active";
+                    break;
                 }
             }
+
         }
     }
 }
